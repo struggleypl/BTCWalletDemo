7119ef6ce42b1a5a7c6767cc5faa520a6b005cea
Jerelin <7973591+struggleypl@users.noreply.github.com>
2022-04-22 17:17:30 +0800

    Update BTCTransferVC.m

--- BTCWalletDemo/CCTBTC/Others/BTCTransferVC.m
+++ BTCWalletDemo/CCTBTC/Others/BTCTransferVC.m
@@ -33,8 +33,7 @@
 - (IBAction)btnAction:(id)sender {
     
     /**
-     需要完整的 CCTBTCLib 库 源代码，请加我的微信：lyp_9527 收费200元（BTC全套库+Demo代码）,支持BTC，USDT交易，签名保证100%通过
-     代码里不明白的都可以问我，其他区块链相关的问题我们也都可以互相讨论，专注区块链开发，还希望交个朋友。
+     需要完整的 CCTBTCLib 库 源代码，请加我的微信：lyp_9527 收费200元（BTC全套库+Demo代码）,支持BTC，USDT交易，签名保证100%通过。
      **/
     
     _pkText.text = @"xxx";
